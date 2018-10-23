113a14db57a0c0489f8396eeed706f1df6559a41
Ezequiel <eze.gimenez.98@gmail.com>
2018-10-23 17:35:00 -0300

    Auto stash before merge of "master" and "origin/master"

--- src/Inteligencias/InteligenciaPowerUpDefecto.java
+++ src/Inteligencias/InteligenciaPowerUpDefecto.java
@@ -5,22 +5,22 @@ import java.awt.Rectangle;
 import power_ups.PowerUp;
 import utilidades.Constantes;
 
-public class InteligenciaPowerUpDefecto extends Inteligencia{
-	
+public class InteligenciaPowerUpDefecto extends Inteligencia {
+
 	private int contador;
-	
+
 	public InteligenciaPowerUpDefecto() {
 		contador = 100;
 	}
-	
+
 	public void actualizarPosicion(PowerUp powerUp) {
 		Rectangle rec = powerUp.getRectangle();
-		
+
 		float newY = rec.y;
-		
-		if(rec.y > Constantes.MAP_HEIGHT - rec.getHeight()) {
+
+		if (rec.y > Constantes.MAP_HEIGHT - rec.getHeight()) {
 			contador--;
-			if(contador == 0) {
+			if (contador == 0) {
 				powerUp.eliminar();
 			}
 			
